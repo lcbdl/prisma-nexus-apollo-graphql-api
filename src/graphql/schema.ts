72bb67c059e35bcd7ae44f8134962c00ac7891d6
chuanbao lu <chuanbao.lu@gmail.com>
2023-12-14 10:48:37 -0500

    add README.md

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,14 +1,14 @@
 import { makeSchema } from 'nexus';
 import { DateTime } from 'nexus-prisma/scalars';
 import { join } from 'path';
-import { Post } from './Post';
-import { AuthType, Gender, User } from './User';
-import { Notification } from './Notification';
+import * as Post from './Post';
+import * as User from './User';
+import * as Notification from './Notification';
 
 export const schema = makeSchema({
-  types: [DateTime, Gender, AuthType, User, Post, Notification], // 1
+  types: [DateTime, User, Post, Notification], 
   outputs: {
-    typegen: join(__dirname, '..', 'nexus-typegen.ts'), // 2
-    schema: join(__dirname, '..', 'schema.graphql') // 3
+    typegen: join(__dirname, 'generated/nexus-typegen.ts'),
+    schema: join(__dirname, 'generated/schema.graphql') 
   }
 });
